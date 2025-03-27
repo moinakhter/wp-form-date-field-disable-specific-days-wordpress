b5c856f72328954e3a6d3d48e19e81300800abe3
Moin Akhtar <39314469+moinakhter@users.noreply.github.com>
2025-03-27 13:00:49 +0300

    Update wp-form-date-disable-days.js

--- wp-form-date-disable-days.js
+++ wp-form-date-disable-days.js
@@ -1,63 +1,37 @@
-  
-// disabling holidays on month change by arrows.
-$(document).delegate(".flatpickr-calendar .flatpickr-next-month,.flatpickr-calendar .flatpickr-prev-month,.wpforms-datepicker-wrap .wpforms-field-date-time-date","click",function () {
+// Disabling holidays on month change using arrows
+$(document).delegate(".flatpickr-calendar .flatpickr-next-month,.flatpickr-calendar .flatpickr-prev-month,.wpforms-datepicker-wrap .wpforms-field-date-time-date","click", function () {
     var month = $(".flatpickr-calendar .flatpickr-monthDropdown-months option:selected").val();
     month = parseInt(month);
-    var addMonth = (month+1);
+    var addMonth = (month + 1);
     checkDay(addMonth);
-  });
-  
-// disabling holidays on month change from dropdown
+});
 
-  $(document).delegate(".flatpickr-calendar .flatpickr-monthDropdown-months","change",function () {
+// Disabling holidays on month change using dropdown
+$(document).delegate(".flatpickr-calendar .flatpickr-monthDropdown-months", "change", function () {
     var month = $(this).val();
     month = parseInt(month);
-    var addMonth = (month+1);
+    var addMonth = (month + 1);
     checkDay(addMonth);
+});
 
-  });
-
-  function checkDay(addMonth){
-      $(".flatpickr-calendar span.flatpickr-day").each(function () {
-          var thisday =  $(this).html();
-          thisday = parseInt(thisday);
-           // disabling holidays day by month
-          if(addMonth == 4){
-              if(thisday == 29){
-                  $(this).addClass("flatpickr-disabled");
-              }
-          }
-          
-          if(addMonth == 5){
-              if(thisday == 3 || thisday == 4 || thisday == 5  ){
-                  $(this).addClass("flatpickr-disabled");
-              }
-          }
-
-          if(addMonth == 7){
-              if(thisday == 18){
-                  $(this).addClass("flatpickr-disabled");
-              }
-          }
-          if(addMonth == 8){
-              if(thisday == 11){
-                  $(this).addClass("flatpickr-disabled");
-              }
-          }
-          if(addMonth == 9){
-              if(thisday == 19 ||  thisday == 23){
-                  $(this).addClass("flatpickr-disabled");
-              }
-          }
-          if(addMonth == 10){
-              if(thisday == 10){
-                  $(this).addClass("flatpickr-disabled");
-              }
-          }
-          if(addMonth == 11){
-              if(thisday == 3 || thisday == 23){
-                  $(this).addClass("flatpickr-disabled");
-              }
-          }
-      });
-  }
+function checkDay(addMonth) {
+    $(".flatpickr-calendar span.flatpickr-day").each(function () {
+        var thisday = $(this).html();
+        thisday = parseInt(thisday);
+        
+        // Disabling specific holiday dates by month
+        var disabledDates = {
+            4: [29],
+            5: [3, 4, 5],
+            7: [18],
+            8: [11],
+            9: [19, 23],
+            10: [10],
+            11: [3, 23]
+        };
+        
+        if (disabledDates[addMonth] && disabledDates[addMonth].includes(thisday)) {
+            $(this).addClass("flatpickr-disabled");
+        }
+    });
+}
